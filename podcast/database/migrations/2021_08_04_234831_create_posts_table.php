59353ef18fdab5966c96e5ea5f875f65d61979d2
Aiden <aiden@MSI.localdomain>
2021-08-05 01:12:20 +0100

    first migration

---
+++ podcast/database/migrations/2021_08_04_234831_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('url');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('episode_number');
+            $table->integer('date_created');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
